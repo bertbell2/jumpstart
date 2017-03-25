b6bb6fee34ec6d7d3112ccdd821f47b46b27adda
bertbell2 <bertbell2@yahoo.com>
2017-03-25 11:26:04 -0700

    Complete animations, finalize

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,27 @@
 // main.js for jumpstart
 $(document).ready( function(){
+  $('.carousel').slick({
+    slidesToScroll: 1,
+    slidesPerRow: 1,
+    slidesToShow: 1,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 4000,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
 
+  var scrollAmount = 0;
+  $(window).scroll( function(){
+    var curTop = $(this).scrollTop();
+    if ( (curTop - scrollAmount) > 50 ) {
+      var navbarH = $('.navbar').outerHeight();
+      $('.navbar').animate({top: '-' + navbarH}, 150);
+      scrollAmount = curTop ;
+    } else if ( scrollAmount - curTop > 50 ) {
+      $('.navbar').animate({top: '0px'}, 150);
+      scrollAmount = curTop;
+    }
+  });
 });
